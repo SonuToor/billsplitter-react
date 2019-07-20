ec12c378f038411dfac5a6e9639bf93c930bec8a
SonuToor <savrooptoor92@gmail.com>
2019-07-20 15:51:25 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import PartyForm from "./components/PartyForm"
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="main-title">Billsplitter!</h1>
+        <PartyForm />
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/PartyForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class PartyForm extends React.Component {
+  render() {
+    return (
+        <form className="party-size-form">
+            <label>How many people?</label>
+        </form>
+    )
+  }
+}
