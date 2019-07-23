35d7a37df7be6c437677dc9b718073790aa522a3
SonuToor <savrooptoor92@gmail.com>
2019-07-22 21:24:19 -0400

    second commit, party form logic complete

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import React from 'react';
 import PartyForm from "./components/PartyForm"
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      partySize : 0
+    }
+  }
+
+  updatePartySize = (size) => {
+    this.setState({ partySize : size })
+  }
+
   render() {
     return (
       <div>
         <h1 className="main-title">Billsplitter!</h1>
-        <PartyForm />
+        <PartyForm onSubmit={this.updatePartySize}/>
       </div>
     )
   }
--- src/components/PartyForm.js
+++ src/components/PartyForm.js
@@ -2,11 +2,39 @@ import React from 'react';
 
 
 export default class PartyForm extends React.Component {
+  constructor() {
+    super()
+    this.state = { partySize : 0}
+  }
+  
+  handleSubmit = (event) => {
+    event.preventDefault();
+    let size = event.target[0].value;
+    this.setState({ partySize : size })
+    this.props.onSubmit(size)
+
+  }
+  createPartyMembersForm = () => {
+    let inputs = []
+
+    for (let i = 0; i < this.state.partySize; i ++) {
+      inputs.push(<input key={i} className="party-member" placeholder="Enter party members name here"></input>)
+    }
+    return inputs
+  }
+
   render() {
     return (
-        <form className="party-size-form">
-            <label>How many people?</label>
-        </form>
+        <div className="party-form">
+          <form className="party-size-form" onSubmit={this.handleSubmit}>
+              <label>How many people are in your party?</label>
+              <input type="number" min="2"></input>
+              <button>Submit</button>
+          </form>
+          <form className="party-members-form">
+            {this.createPartyMembersForm()}
+          </form>
+        </div>
     )
   }
 }
