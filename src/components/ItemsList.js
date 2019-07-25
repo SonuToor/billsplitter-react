3969499bdfd1794b0d61bc398faee48beb011e4c
SonuToor <savrooptoor92@gmail.com>
2019-07-25 19:37:37 -0400

    All inputs being succesfully received, final logic is the next step

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -9,8 +9,23 @@ import PartyButtons from "./PartyButtons"
 
 export default class ItemsList extends React.Component {
 
+    handlePartyButtonClick = (event) => {
+        // TO DO!
+            // Look at the Clio notebook first and plan out the order of operations. 
+            
+            // Need to return a data structure to App.js
+                // the data structure will be used to calculate totals and to generate the table to display totals
+
+                // what structure for this purpose?
+
+            // Should the buttons be rendered disabled after it's clicked?
+                // Why or why not?
+        let user = event.target.innerHTML
+
+    }
+
     render() {
-        let userButtons = <PartyButtons members={this.props.party} />
+        let userButtons = <PartyButtons members={this.props.party} handleClick={this.handlePartyButtonClick}/>
         return (
             <div>
                 <h2 style={{textAlign : "center"}}>Your Items!</h2>
--- src/components/PartyButtons.js
+++ src/components/PartyButtons.js
@@ -3,11 +3,9 @@ import React from 'react';
 import {Button, ButtonGroup} from 'react-bootstrap'
 
 export default class PartyButtons extends React.Component {
-    
     render() {
         return(
-            // the listener and selected button will be sent from here to the itemsList
-            <ButtonGroup>
+            <ButtonGroup onClick={this.props.handleClick}>
                 {this.props.members[0].map((member, i) =>
                 <Button variant="secondary" key={i}>{member}</Button>
                 )}
